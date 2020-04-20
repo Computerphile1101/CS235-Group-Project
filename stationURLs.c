4128dcbcef20b2f95493054c3a98f200d168ea57
Computerphile1101 <61714825+Computerphile1101@users.noreply.github.com>
2020-04-20 13:18:47 -0500

    Add files via upload

---
+++ stationURLs.c
@@ -0,0 +1,53 @@
+/*This file holds the 14 URLs to the XML data of each location's weather station. Locations are geographically based across the state of Iowa.*/
+ 
+//Ames
+https://w1.weather.gov/xml/current_obs/KAMW.xml
+
+//Burlington
+https://w1.weather.gov/xml/current_obs/KBRL.xml
+
+//Cedar Rapids
+https://w1.weather.gov/xml/current_obs/KCID.xml
+
+//Council Bluffs
+https://w1.weather.gov/xml/current_obs/KCBF.xml
+
+//Davenport
+https://w1.weather.gov/xml/current_obs/KDVN.xml
+
+//Des Moines
+https://w1.weather.gov/xml/current_obs/KDSM.xml
+
+//Dubuque
+https://w1.weather.gov/xml/current_obs/KDBQ.xml
+
+//Fort Dodge
+https://w1.weather.gov/xml/current_obs/KFOD.xml
+
+//Iowa City
+https://w1.weather.gov/xml/current_obs/KIOW.xml
+
+//Mason City
+https://w1.weather.gov/xml/current_obs/KMCW.xml
+
+//Ottumwa
+https://w1.weather.gov/xml/current_obs/KOTM.xml
+
+//Sioux City
+https://w1.weather.gov/xml/current_obs/KSUX.xml
+
+//Spencer
+https://w1.weather.gov/xml/current_obs/KSPW.xml
+
+//Waterloo
+https://w1.weather.gov/xml/current_obs/KALO.xml
+
+
+
+
+
+
+
+
+
+
