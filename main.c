4576842584efb7a72176307c94a4698b052e1bcc
Keegan Hutchins <keeganrock@live.com>
2020-04-20 20:57:25 -0500

    Added the main.c file
    For this file, when you compile and run, the scanner will ask for a state abbreviation. Type in IA for Iowa. Potentially, whatever 2 characters the user types in, it could load all of the weather stations for them to pick from. In the HTTP get request, it says "state=ia". The IA part can be replaced with any state abbreviation and it can load all of the weather stations for the user to pick. It's not fully correct or even partial but it works. Maybe this can get us started with it.

---
+++ main.c
@@ -0,0 +1,84 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char *argv){
+	
+	char state[3]; //includes \n
+	printf("Please enter your State\nEX. (IA)\n");
+	scanf("%s", state);	//Have them enter what state they want to display.
+	printf("You entered: %s\n", state); //displays state
+	
+	/* first what are we going to send and where are we going to send it? */
+    int portno =        80;
+    char *host;
+	host = "https://w1.weather.gov/xml/current_obs/seek.php?state=ia&Find=Find\n";
+    char *message_fmt = "GET /xml/current_obs/KCID.xml HTTP/1.0\r\nHost: www.weather.gov\r\nConnection: close\r\nUser-Agent: /1.0\r\nAccept: */";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, received, total;
+    char message[1024],response[4096];
+
+    /* fill in the parameters */
+    sprintf(message,message_fmt,argv[1],argv[2]);
+    printf("Request:\n%s\n",message);
+
+    /* create the socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+
+    /* lookup the ip address */
+    server = gethostbyname(host);
+    if (server == NULL) error("ERROR, no such host");
+
+    /* fill in the structure */
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    /* connect the socket */
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    /* send the request */
+    total = strlen(message);
+    sent = 0;
+    do {
+        bytes = write(sockfd,message+sent,total-sent);
+        if (bytes < 0)
+            error("ERROR writing message to socket");
+        if (bytes == 0)
+            break;
+        sent+=bytes;
+    } while (sent < total);
+
+    /* receive the response */
+    memset(response,0,sizeof(response));
+    total = sizeof(response)-1;
+    received = 0;
+    do {
+        bytes = read(sockfd,response+received,total-received);
+        if (bytes < 0)
+            error("ERROR reading response from socket");
+        if (bytes == 0)
+            break;
+        received+=bytes;
+    } while (received < total);
+
+    if (received == total)
+        error("ERROR storing complete response from socket");
+
+    /* close the socket */
+    close(sockfd);
+
+    /* process response */
+    printf("Response:\n%s\n",response);
+}
