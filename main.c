a4a437d5a19279d705b26071c0a3dce31bfa6c75
Keegan Hutchins <keeganrock@live.com>
2020-04-27 13:38:03 -0500

    Add files via upload
    New version. Trying to get the GET request working

--- main.c
+++ main.c
@@ -7,14 +7,46 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <netinet/tcp.h>
+
+int socket_connect(char *host, in_port_t port){
+	struct hostent *hp;
+	struct sockaddr_in addr;
+	int on = 1, sock;     
+
+	if((hp = gethostbyname(host)) == NULL){
+		herror("gethostbyname");
+		exit(1);
+	}
+	bcopy(hp->h_addr, &addr.sin_addr, hp->h_length);
+	addr.sin_port = htons(port);
+	addr.sin_family = AF_INET;
+	sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (const char *)&on, sizeof(int));
+
+	if(sock == -1){
+		perror("setsockopt");
+		exit(1);
+	}
+	
+	if(connect(sock, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1){
+		perror("connect");
+		exit(1);
+
+	}
+	return sock;
+}
+
+#define BUFFER_SIZE 1024
 
 int main(int argc, char *argv){
 	
 char station_number;
 int portno = 80;
 char *host;
-host = "https://w1.weather.gov/xml/current_obs/seek.php?state=ia&Find=Find\n";
+host = "w1.weather.gov";
 char *message_fmt;
+char buffer[BUFFER_SIZE];
 
 //Creates a "menu" and asks user to choose a weather station.
 printf("Choose a weather station from the following list: (Enter a-n)\n");
@@ -26,11 +58,14 @@ printf("(i) Iowa City\n(j) Mason City\n");
 printf("(k) Ottumwa\n(l) Sioux City\n");
 printf("(m) Spencer\n(n) Waterloo\n");
 
+int fd;
+
 //Waits for user's choice to be a valid input.
+fd = socket_connect(host, portno);
+
 do {
 
 scanf(" %c", &station_number);
-
 //After a-n is entered, choose the corresponding URL.
     switch(station_number){
 
@@ -116,9 +151,12 @@ message_fmt = "GET /xml/current_obs/KALO.xml HTTP/1.0\r\nHost:www.weather.gov\r\
          (station_number != 'j')&&(station_number != 'k')&&(station_number != 'l')&&
          (station_number != 'm')&&(station_number != 'n'));
 
+write(fd, message_fmt, strlen(message_fmt)); // write(fd, char[]*, len);
+read(fd, buffer, sizeof(buffer)); 
+printf("From Server : %s", buffer); 
 
 //Rest of main code starts here.....
-
+printf("Message: %s\n", message_fmt);
 
 
 }// end of main
@@ -187,4 +225,3 @@ message_fmt = "GET /xml/current_obs/KALO.xml HTTP/1.0\r\nHost:www.weather.gov\r\
     // process response
     printf("Response:\n%s\n",response);
 */
-
